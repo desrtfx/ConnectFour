c65774651eba456af16c6b9f908f1e3b7009b737
desrtfx <desrtfx@gmail.com>
2014-11-26 16:40:45 +0100

    FollowStrategy implemented

--- ConnectFour/src/connectfour/FollowStrategy.java
+++ ConnectFour/src/connectfour/FollowStrategy.java
@@ -1,11 +1,45 @@
 package connectfour;
 
+import java.util.Random;
+
 public class FollowStrategy implements MoveStrategy {
+
+	private Board oldBoard;
+	private Random rnd;
 
 	@Override
 	public int makeMove(Board board) {
-		// TODO Auto-generated method stub
-		return 0;
+		int col = 0;
+		if (oldBoard != null) {
+			// not the first move for the computer player
+			// generate a new (empty) board with the same
+			// dimensions as the actual game board, but empty
+			oldBoard = new Board(board.getRows(), board.getCols());
+		}
+		col = getDiff(board);
+
+		if ((col == -1) || (!board.isValid(col))) {
+			// Either empty board (col = -1) or
+			// column is already full,
+			// do random move
+			do {
+				col = rnd.nextInt(board.getCols());
+			} while (!board.isValid(col));
+		}
+
+		oldBoard = new Board(board); // Use the copy constructor
+		return col;
+	}
+
+	private int getDiff(Board board) {
+		for (int row = 0; row < board.getRows(); row++) {
+			for (int col = 0; col < board.getCols(); col++) {
+				if (oldBoard.getValueAt(row, col) != board.getValueAt(row, col)) {
+					return col; // difference found, return col
+				}
+			}
+		}
+		return -1; // no difference found
 	}
 
 }
