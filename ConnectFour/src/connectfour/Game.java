986cd945c263e2b429c6b45773d0a74727e8fb60
desrtfx <desrtfx@gmail.com>
2014-11-27 11:19:18 +0100

    First game loop routines implemented

--- ConnectFour/src/connectfour/Game.java
+++ ConnectFour/src/connectfour/Game.java
@@ -1,48 +1,129 @@
 package connectfour;
 
+import java.util.Random;
+
 import connectfour.assets.Board;
 import connectfour.assets.Player;
-import connectfour.strategy.*;
+import connectfour.gui.TxtGUI;
+import connectfour.strategy.HumanStrategy;
 
 public class Game {
-	
+
+	// GameState constants
+	private static final int NO_WINNER = -1;
+	private static final int QUIT = -2;
+	private static final int WINNER_PLAYER_1 = 0;
+	private static final int WINNER_PLAYER_2 = 1;
+	private static final int GAME_TIED = 2;
+
+	// Game variables
 	private Board board;
 	private int winCond;
 	private int rows;
 	private int cols;
 	private Player[] players;
-	
-	/** Constructs a default game of connect four.<BR>
-	 *  Human vs. Human on a default 6 row, 7 column board<br>
-	 *  with 4 consecutive chips to win
+	private Random rnd = new Random();
+	TxtGUI gui;
+
+	/**
+	 * Constructs a default game of connect four.<BR>
+	 * Human vs. Human on a default 6 row, 7 column board<br>
+	 * with 4 consecutive chips to win
 	 */
-	public Game() { 
+	public Game() {
 		this.rows = 6;
 		this.cols = 7;
-		board = new Board(rows,cols);
+		board = new Board(rows, cols);
+		gui = new TxtGUI(board);
 		this.winCond = 4;
 		players = new Player[2];
 		players[0] = new Player(1, new HumanStrategy());
 		players[1] = new Player(2, new HumanStrategy());
 	}
-	
-	/**Constructs a new custom connect four game
-	 * @param players Array of 2 players that play the game
-	 * @param board The Gameboard
-	 * @param winCond the winning condition
+
+	/**
+	 * Constructs a new custom connect four game
+	 * 
+	 * @param players
+	 *            Array of 2 players that play the game
+	 * @param board
+	 *            The Gameboard
+	 * @param winCond
+	 *            the winning condition
 	 */
 	public Game(Player[] players, Board board, int winCond) {
 		this.players = players;
 		this.board = board;
+		gui = new TxtGUI(this.board);
 		this.winCond = winCond;
 		this.rows = board.getRows();
 		this.cols = board.getCols();
 	}
-	
+
 	public void init() {
 		board.init();
 		players[0].init();
 		players[1].init();
+	}
+
+	public void run() {
+
+		// Random player starts
+		int activePlayer = rnd.nextInt(2);
+		int winner = NO_WINNER;
+		int move;
+
+		// Main game loop
+		do {
+
+			// Display the board
+			gui.drawBoard();
+
+			// Query player move
+			move = players[activePlayer].makeMove(players[activePlayer],
+					players[1 - activePlayer], board);
+
+			// Execute the move on the board
+			board.dropChip(activePlayer, move);
+
+			// TODO: Check for winning conditions
+			if ((board.isFull()) && (winner == NO_WINNER)) {
+				winner = GAME_TIED;
+			}
+			// Other conditions here
+
+			// next player
+			activePlayer = 1 - activePlayer;
+
+		} while (winner == NO_WINNER); // End main game loop
+
+		// Draw the board one final time if the players haven't quit
+		if (winner != QUIT) {
+			gui.drawBoard();
+		}
+
+		switch (winner) {
+		case QUIT:
+			// TODO: display game tied message
+			break;
+		case NO_WINNER:
+			// Should not be reached, only here for completeness
+			break;
+		case WINNER_PLAYER_1:
+			// TODO: Print player 1 won message
+			break;
+		case WINNER_PLAYER_2:
+			// TODO: Print player 2 won message
+			break;
+		case GAME_TIED:
+			// TODO: display game tied message
+			break;
+		default:
+			// Uh-Oh something went wrong - this should never be reached
+			throw new UnsupportedOperationException(
+					"Illegal Game State, not supported");
+		}
+
 	}
 
 }
--- ConnectFour/src/connectfour/assets/Board.java
+++ ConnectFour/src/connectfour/assets/Board.java
@@ -1,6 +1,5 @@
 package connectfour.assets;
 
-
 public class Board {
 
 	// the actual game board
@@ -10,6 +9,8 @@ public class Board {
 	private final int rows;
 	private final int cols;
 
+	private int chipCount;
+
 	public Board() {
 		this(6, 7);
 	}
@@ -18,15 +19,20 @@ public class Board {
 		this.rows = rows;
 		this.cols = cols;
 		board = new int[this.rows][this.cols];
+		chipCount = 0;
 	}
 
 	public Board(Board board) {
 		this.rows = board.getRows();
 		this.cols = board.getCols();
 		this.board = new int[this.rows][this.cols];
+		chipCount = 0;
 		for (int row = 0; row < rows; row++) {
 			for (int col = 0; col < cols; col++) {
 				this.board[row][col] = board.getValueAt(row, col);
+				if (this.board[row][col] != 0) {
+					chipCount++;
+				}
 			}
 		}
 	}
@@ -64,6 +70,18 @@ public class Board {
 			throw new IllegalArgumentException("The supplied value is invalid.");
 		}
 		board[row][col] = value;
+		updateChipCount();
+	}
+
+	private void updateChipCount() {
+		chipCount = 0;
+		for (int row = 0; row < rows; row++) {
+			for (int col = 0; col < cols; col++) {
+				if (board[row][col] != 0) {
+					chipCount++;
+				}
+			}
+		}
 	}
 
 	public void setValueAt(Cell cell, int value) {
@@ -77,6 +95,11 @@ public class Board {
 	public int getRows() {
 		return rows;
 	}
+	
+	// returns the chipCount to speed winner analysis
+	public int getChipCount() {
+		return chipCount;
+	}
 
 	public int[] getRow(int row) {
 		if ((row < 0) || (row >= rows)) {
@@ -85,21 +108,38 @@ public class Board {
 		}
 		return board[row];
 	}
-	
+
 	public boolean isValid(int col) {
-		return (board[rows-1][col]==0);
+		return (board[rows - 1][col] == 0);
+	}
+
+	public void dropChip(int player, int col) {
+		for (int i = 0; i < rows; i++) {
+			if (board[i][col] == 0) {
+				board[i][col] = player;
+				break;
+			}
+		}
+	}
+
+	public boolean isEmpty() {
+		return (chipCount == 0);
+	}
+
+	public boolean isFull() {
+		return (chipCount == (rows * cols));
 	}
 
 	/**
 	 * 
 	 */
 	public void init() {
-		for(int row = 0; row<rows;row++) {
-			for(int col = 0; col<cols; col++) {
+		for (int row = 0; row < rows; row++) {
+			for (int col = 0; col < cols; col++) {
 				board[row][col] = 0;
 			}
 		}
-		
+		chipCount = 0;
 	}
 
 }
